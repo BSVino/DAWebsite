283f0145e73d0546f154a8356a5d40820c743602
Jorge Rodriguez <jorge@lunarworkshop.com>
2014-06-19 12:27:03 -0700

    Contact link

--- htdocs/wp/wp-content/themes/doubleaction/footer.php
+++ htdocs/wp/wp-content/themes/doubleaction/footer.php
@@ -12,7 +12,7 @@
 <?php wp_footer(); ?>
 
 <div id="footer" role="contentinfo">
-<div class="sitemap"><a class="whitelink" href="/blog">NEWS</a> <?/*| <a class="whitelink" href="/press">PRESS</a> | <a class="whitelink" href="/about">ABOUT</a> | <a class="whitelink" href="/contact">CONTACT</a>*/?></div><br />
+<div class="sitemap"><a class="whitelink" href="/blog">NEWS</a> <?/*| <a class="whitelink" href="/press">PRESS</a> | <a class="whitelink" href="/about">ABOUT</a>*/?> | <a class="whitelink" href="/contact">CONTACT</a></div><br />
 <!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
         <p>
                 <?php bloginfo('name'); ?> is proudly powered by <a class="whitelink" href="http://wordpress.org/">WordPress</a>
