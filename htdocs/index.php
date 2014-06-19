3a3bded7e1cbcfb2f8798e58dd9f79e664622c3c
Jorge Rodriguez <jorge@lunarworkshop.com>
2014-06-19 10:58:25 -0700

    Don't show Facebook likes for pages

--- htdocs/index.php
+++ htdocs/index.php
@@ -84,17 +84,19 @@
 							?>
 				        </div>
 
-						<div class="share-post"><ul><li>
-							<div id="fb-root"></div>
-							<script>(function(d, s, id) {
-								var js, fjs = d.getElementsByTagName(s)[0];
-								if (d.getElementById(id)) return;
-								js = d.createElement(s); js.id = id;
-								js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-								fjs.parentNode.insertBefore(js, fjs);
-							}(document, 'script', 'facebook-jssdk'));</script>
-							<div class="fb-like" data-href="<? the_permalink(); ?>" data-send="false" data-layout="button_count" data-width="90" data-show-faces="false"></div>
-						</li></ul></div>
+						<? if (!is_page()) { ?>
+							<div class="share-post"><ul><li>
+								<div id="fb-root"></div>
+								<script>(function(d, s, id) {
+									var js, fjs = d.getElementsByTagName(s)[0];
+									if (d.getElementById(id)) return;
+									js = d.createElement(s); js.id = id;
+									js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+									fjs.parentNode.insertBefore(js, fjs);
+								}(document, 'script', 'facebook-jssdk'));</script>
+								<div class="fb-like" data-href="<? the_permalink(); ?>" data-send="false" data-layout="button_count" data-width="90" data-show-faces="false"></div>
+							</li></ul></div>
+						<? } ?>
 
 						<div style="height: 1px; background-color: black; margin-top: 60px;"></div>
 
