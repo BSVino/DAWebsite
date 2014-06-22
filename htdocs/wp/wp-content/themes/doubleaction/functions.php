e2ffbb1d6c5a8b56b4a6729610643ea409e9bfff
Jorge Rodriguez <jorge@lunarworkshop.com>
2014-06-21 22:47:24 -0700

    Remove unused font to increase load times

--- htdocs/wp/wp-content/themes/doubleaction/functions.php
+++ htdocs/wp/wp-content/themes/doubleaction/functions.php
@@ -210,9 +210,9 @@ function twentyfourteen_font_url() {
 	 * Translators: If there are characters in your language that are not supported
 	 * by Lato, translate this to 'off'. Do not translate into your own language.
 	 */
-	if ( 'off' !== _x( 'on', 'Lato font: on or off', 'twentyfourteen' ) ) {
-		$font_url = add_query_arg( 'family', urlencode( 'Lato:300,400,700,900,300italic,400italic,700italic' ), "//fonts.googleapis.com/css" );
-	}
+	//if ( 'off' !== _x( 'on', 'Lato font: on or off', 'twentyfourteen' ) ) {
+	//	$font_url = add_query_arg( 'family', urlencode( 'Lato:300,400,700,900,300italic,400italic,700italic' ), "//fonts.googleapis.com/css" );
+	//}
 
 	return $font_url;
 }
